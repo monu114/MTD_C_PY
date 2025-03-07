9951af9fa7e79eac536c1fffb9c3e899e58d3e1e
monu114 <monishy56@gmail.com>
2025-03-07 11:54:06 +0530

    updated

---
+++ day2/code2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int inputNum=0;
+    puts("enter the number to check:- ");
+    scanf("%d",&inputNum);
+    int root =floor(sqrt(inputNum));
+    if(root*root==inputNum)
+        printf("%d id the perfect square");
+    else   
+        printf("%d is not the perfect square");
+}
