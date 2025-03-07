d4065f5eae92f1b4be473d120d874f6de2052b2e
monu114 <monishy56@gmail.com>
2025-03-07 10:21:59 +0530

    day2 and Note file is added

--- day2/code1.c
+++ day2/code1.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    
+    int m=3,n=-2,c=8;
+    n++;
+    printf("%d %d %d\n",m,n,c);
+    ++n;
+    printf("%d %d %d\n",m,n,c);
+    c=--m;
+    printf("%d %d %d\n",m,n,c);
+    n=++c;
+    printf("%d %d %d\n",m,n,c);
+
+
 }
