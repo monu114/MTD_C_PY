8da29678c08400c70b8c2d7be0b55462be4f4a78
monu114 <monishy56@gmail.com>
2025-03-07 12:52:22 +0530

    updated

---
+++ day2/code4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n1=89;
+    int n2=067;
+    int n3=0x99;
+    printf("%d %i %o %x\n",n1,n1,n1,n1);
+    printf("%d %i %o %x\n",n2,n2,n2,n2);
+    printf("%d %i %o %x\n",n3,n3,n3,n3);
+
+}
